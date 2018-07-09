65b120832d46775d538839ab8724788be2858827
Pooja Devray <poojadevray@gmail.com>
2018-07-09 16:31:55 +0530

    Initial commit

---
+++ ExpenseCalculation/src/main/java/com/synerzip/ExpenseCalculation/ExpenseCalculationApplication.java
@@ -0,0 +1,12 @@
+package com.synerzip.ExpenseCalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseCalculationApplication.class, args);
+	}
+}
